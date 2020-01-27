37739f8c12f952945c1f6b919c875e81fdc4c4be
COLIN Jules <julesc5614029@gmail.com>
2020-01-27 12:57:24 +0100

    boutique

---
+++ master.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+int choix;
+int argent = 30;
+
+char *achetable[] = {
+"biere",
+"eau",
+"gateau",
+"épée",
+"bouclier",
+"casque",
+"armure",
+"potion",
+};
+
+int achat (){
+
+}
+int vente() {
+
+}
+
+int main(){
+
+printf("vous avez %d pieces d'Or\n", argent);
+printf("\n");
+printf("bonjour voyageur !\n");
+printf("que souhaitez-vous faire ?\nAcheter: 1 Vendre: 2 Regarder l'inventaire: 3\n");
+scanf("%d\n", choix);
+ if (choix == 1)
+
+
+	return choix;
+}
