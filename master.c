2c81de3af30e378eeae91476fe1a6285a1bcdb96
julesc35 <59653303+julesc35@users.noreply.github.com>
2020-02-01 17:34:49 +0100

    grosse correction

--- master.c
+++ master.c
@@ -11,17 +11,7 @@ struct Achetable{
 	int numero;
 	char NomItem;
 	int PrixItem;
-}
-
-Achetable ListeAchetable [20] 
-ListeAchetable[0]{1,"Epée",100"\n"};
-ListeAchetable[1]{2,"Armure",250"\n"};
-ListeAchetable[2]{3,"Os",5"\n"};
-ListeAchetable[3]{4,"Biere",2"\n"};
-ListeAchetable[4]{5,"entre-cote",10"\n"};
-ListeAchetable[5]{6,"épée",100"\n"};
-ListeAchetable[6]{6,"épée",100"\n"};
-
+};
 
 // initialisation de l'inventaire du joueur
 
@@ -29,17 +19,26 @@ struct Inventaire{
 	int numero;
 	char Nom;
 	int Prix;
-}
-
-Inventaire inventaire [20]
-inventaire[0]{1,"Tasse",2};
-inventaire[1]{2,"Epee Rouillee",15};
-inventaire[2]{3,"Armure Abimee",50};
-inventaire[3]{4,"Arc",20};
-inventaire[4]{5,"Viande Avariee",2};
-inventaire[5]{6,"Pierre Precieuse",500};
+};
 
 int main(){
+
+struct Achetable ListeAchetable [20] (int numero, char NomItem,int PrixItem);
+ListeAchetable[0](1,"Epée",100);
+ListeAchetable[1](2,"Armure",250);
+ListeAchetable[2](3,"Os",5);
+ListeAchetable[3](4,"Biere",2);
+ListeAchetable[4](5,"entre-cote",10);
+ListeAchetable[5](6,"épée",100);
+ListeAchetable[6](6,"épée",100);
+
+struct Inventaire inventaire [20] (int numero, char Nom,int Prix);
+inventaire[0](1,"Tasse",2);
+inventaire[1](2,"Epee Rouillee",15);
+inventaire[2](3,"Armure Abimee",50);
+inventaire[3](4,"Arc",20);
+inventaire[4](5,"Viande Avariee",2);
+inventaire[5](6,"Pierre Precieuse",500);
 
 printf("vous avez %d pieces d'Or\n", argent);
 printf("\n");
@@ -48,7 +47,7 @@ printf("que souhaitez-vous faire ?\nAcheter: 1 Vendre: 2 Regarder l'inventaire: 
 scanf("%d\n", choix);
 
  if (choix == 1){
-	printf("que voiulez-vous acheter ?\n")
+	printf("que voiulez-vous acheter ?\n");
 	printf("%d""%s""%d\n",ListeAchetable);
 	scanf("%d", ListeAchetable.numero);
 return choix;
@@ -57,7 +56,7 @@ return choix;
 if (choix == 2){
 	printf("que voulez-vous vendre ?\n");
 	printf("%d""%s""%d""\n", inventaire);
-	scand("%d", inventaire.numero);
+	scanf("%d", inventaire.numero);
 return choix;
 }
 
